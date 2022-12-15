4c4759c885cfc591155ac42af47511f364a1a986
ranjankumar9 <107936455+ranjankumar9@users.noreply.github.com>
2022-12-15 07:55:24 +0530

    a

---
+++ src/Pages/PaymentSuccess.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import styles from "./PaymentSuccess.module.css";
+import { useNavigate } from "react-router-dom";
+
+const PaymentSuccess = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.Main}>
+      <div className={styles.placed}>
+        <h2 className={styles.heading} style={{ color: "maroon" }}>
+          Order Placed Successfully
+        </h2>
+
+        <h2 className={styles.heading} style={{ color: "maroon" }}>
+          Thank You!
+        </h2>
+
+        <img
+          src="https://mrhomez.in/wp-content/uploads/2020/11/tick.gif"
+          alt="encrypted"
+          className={styles.logo}
+        />
+
+        <button
+          type="submit"
+          className={styles.morebtn}
+          onClick={() => navigate("/")}
+        >
+          Go to Home
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default PaymentSuccess;

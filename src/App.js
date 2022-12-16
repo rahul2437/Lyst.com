e6e6a76a626983d2776e652494e3081852aa0ff3
Pushpendra Singh <erpushpendrasingh8900@gmail.com>
2022-12-16 12:06:44 +0530

    Product page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
      return (
           <div className="App">
                {/* <MainRoutes /> */}
-               <Products />
+               {/* <Products /> */}
           </div>
      );
 }
--- src/Components/Productspage.jsx
+++ src/Components/Productspage.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
 import { getProduct } from "../Redux/AppReducer/action";
 import ProductCard from "./ProductCard";
 
@@ -7,7 +8,7 @@ const Productspage = () => {
      const dispatch = useDispatch();
      const data = 10;
      const { product } = useSelector((store) => store.AppReducer);
-     console.log("product:", product);
+     // console.log("product:", product);
      useEffect(() => {
           if (product.length === 0) {
                dispatch(getProduct());
@@ -15,12 +16,17 @@ const Productspage = () => {
      }, [product.length, dispatch]);
      return (
           <>
-               <ProductCard />
-               <ProductCard />
-               <ProductCard />
-               <ProductCard />
+               
+               {product.length > 0 &&
+                    product?.map((item) => {
+                         // console.log("item:", item);
+                         return <ProductCard key={item.id} item={item} />;
+                    })}
           </>
      );
 };
 
+// const productWrapper = styled.div`
+//      width: 300px;
+// `;
 export default Productspage;
